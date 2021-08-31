14a08ee2f710d7274ef18ba3219f3b4e21b81860
lnt <lalit.tanwar07@gmail.com>
2021-08-31 06:19:15 +0000

    Deploying to dev from @ mehery-soccom/mehery-web@2cb28a429a809968e2383d4ae735f0d2aea240d5 🚀

---
+++ legacy/js/login.js
@@ -0,0 +1,62 @@
+$( document ).ready(function() {
+    var w = window.innerWidth;
+
+    if(w > 767){
+        $('#menu-jk').scrollToFixed();
+    }else{
+        $('#menu-jk').scrollToFixed();
+    }
+})
+
+
+$( document ).ready(function() {
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 5000,
+        navText:['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+    function loginSubmit() {
+         return $.post(window.CONST.CONTEXT + "/auth/login/submit",{
+             username :   $("#login-username").val(),
+             password :  $("#login-password").val(),
+             rememberme : $("#rememberme").is(":checked")
+         }).done(function(rsp){
+             console.log(rsp);
+             if(rsp.data && (rsp.data.success || rsp.data == 'success'))
+                 location.href = window.CONST.CONTEXT + "/app/home";
+             else {
+                 $("#login_error").text(rsp.message);
+             }
+         });
+    }
+
+
+    $("#btn-login").click(function(){
+        return loginSubmit();
+    });
+
+    $('.login-det').on('keypress', function(e) {
+        var code = e.keyCode || e.which;
+        if(code==13){
+            return loginSubmit();
+        }
+    });
+
+});
